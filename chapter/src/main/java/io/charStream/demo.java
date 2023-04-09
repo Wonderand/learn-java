507030382eca30f7e7b933ccdc60734904ec369a
huzhirong <2290641663@qq.com>
2023-04-09 18:30:23 +0800

    char file

---
+++ chapter/src/main/java/io/charStream/demo.java
@@ -0,0 +1,23 @@
+package io.charStream;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class demo {
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader fileReader = new FileReader("d.txt");
+        fileReader.read();
+        FileWriter fileWriter = new FileWriter("d.txt");
+
+        int len;
+        while ((len = fileReader.read())!=-1){
+            System.out.println(len);
+        }
+
+        fileWriter.close();
+        fileReader.close();
+    }
+}
