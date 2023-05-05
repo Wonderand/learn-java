4afb3e8082c714a1be016a5db8c0304172c884c0
huzhirong <2290641663@qq.com>
2023-05-05 10:13:36 +0800

    add utils

---
+++ ssm/src/test/java/com/huzhirong/top/ssm/SsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huzhirong.top.ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
