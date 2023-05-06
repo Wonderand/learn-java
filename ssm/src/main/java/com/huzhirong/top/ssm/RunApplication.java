49eb5f10b0a24397ef4f62159199d92831eda51c
huzhirong <2290641663@qq.com>
2023-05-06 23:24:29 +0800

    log

--- ssm/src/main/java/com/huzhirong/top/ssm/RunApplication.java
+++ ssm/src/main/java/com/huzhirong/top/ssm/RunApplication.java
@@ -3,10 +3,8 @@ package com.huzhirong.top.ssm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan(value = "com.huzhirong.top.ssm.modules.*.mapper")
 public class RunApplication {
 
     public static void main(String[] args) {
