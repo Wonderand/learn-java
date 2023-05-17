445ea0a8dda5f07e9d161424cd75c256a1c9c7f5
huzhirong <2290641663@qq.com>
2023-05-17 12:09:45 +0800

    add str

---
+++ chapter/src/main/java/str/api/utils.java
@@ -0,0 +1,21 @@
+package str.api;
+
+
+/**
+ * 字符串工具类
+ */
+public class utils {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 末尾追加字符串
+     */
+    public static String append(String str, String appendStr) {
+        str.concat(appendStr);
+        return str;
+    }
+
+}
---
+++ chapter/src/main/java/str/test.java
@@ -0,0 +1,12 @@
+package str;
+
+import java.util.Arrays;
+
+public class test {
+    public static void main(String[] args) {
+
+		char[] chars = {'a','b','c','d','e','f','g','h','i','j'};
+        String s = new String(chars);
+		System.out.println(s);
+    }
+}
