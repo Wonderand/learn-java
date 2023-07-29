d899bec7d591bbd7c21ae62c87753def3fc45917
hzr <2290641663@qq.com>
2023-07-29 16:14:07 +0800

    class01 learn

--- chapter/src/main/java/reflection/class_/class01.java
+++ chapter/src/main/java/reflection/class_/class01.java
@@ -4,7 +4,19 @@ import reflection.pojo.Cat;
 
 public class class01 {
     public static void main(String[] args) {
-        //看看class类图
-        Cat cat = new Cat();
+        //1.通过类名.class获取
+        try {
+            Cat cat = new Cat();
+            //对于某个类的class对象，在内存中只有一份，因为类只加载一次
+            Class<?> aClass = Class.forName("reflection.pojo.Cat");
+            Class<?> aClass1 = Class.forName("reflection.pojo.Cat");
+            System.out.println(cat.getClass().hashCode());
+            System.out.println(aClass.hashCode());
+            System.out.println(aClass1.hashCode());
+            Class<?> aClass2 = Class.forName("reflection.pojo.Dog");
+            System.out.println(aClass2.hashCode());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
