eb18978256a71f01a6c3c2d93bf35c14c12365b7
hzr <2290641663@qq.com>
2024-01-18 11:51:33 +0800

    添加读取文件夹方法

--- chapter/src/main/java/io/base64_img/test.java
+++ chapter/src/main/java/io/base64_img/test.java
@@ -1,8 +1,10 @@
 package io.base64_img;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Test;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.UUID;
@@ -10,7 +12,8 @@ import java.util.UUID;
 public class test {
 
     public static void main(String[] args) {
-//        String s = base64Img("E:\\jdk17\\test\\chapter\\src\\main\\resources\\1.jpg");
+        String s = base64Img("D:\\yitizi\\yitizi\\sword\\sworda\\sa00001\\001.jpg");
+        System.out.println(s);
 //        try (FileOutputStream fileOutputStream = new FileOutputStream("E:\\jdk17\\test\\chapter\\src\\main\\resources\\1.txt")){
 //            //头部加入data:image/png;base64,
 //            //FileUtils.writeStringToFile(new File("E:\\jdk17\\test\\chapter\\src\\main\\resources\\1.jpg"),s,"UTF-8");
@@ -20,8 +23,47 @@ public class test {
 //        } catch (IOException e) {
 //            throw new RuntimeException(e);
 //        }
-        String file ="E:\\jdk17\\test\\chapter\\src\\main\\resources\\1.txt";
-        base64ToImg(file,"E:\\jdk17\\test\\chapter\\src\\main\\resources\\2.jpg");
+//        String file ="E:\\jdk17\\test\\chapter\\src\\main\\resources\\1.txt";
+//        base64ToImg(file,"E:\\jdk17\\test\\chapter\\src\\main\\resources\\2.jpg");
+    }
+
+    //读取文件夹下的所有jpg文件的路径
+    @Test
+    public void test1() throws IOException {
+        //创建字符缓冲输出流对象
+        BufferedWriter bw = new BufferedWriter(new FileWriter("E:\\jdk17\\test\\chapter\\src\\main\\file\\path.txt", true));
+//        //写数据
+//        bw.write("123");
+//        bw.newLine();
+//        bw.write("456");
+//        //释放资源
+//        bw.close();
+        File file = new File("D:\\yitizi\\yitizi\\sword");
+        ArrayList<String> jpgFiles = new ArrayList<>();
+        getJpgFiles(file.getAbsolutePath(),jpgFiles);
+        for (int i = 0; i < jpgFiles.size(); i++) {
+//            System.out.println(jpgFiles.get(i));
+            bw.write(jpgFiles.get(i));
+            bw.newLine();
+        }
+        bw.close();
+    }
+
+    public static void getJpgFiles(String path, ArrayList<String> jpgFiles) {
+        File folder = new File(path);
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    getJpgFiles(file.getAbsolutePath(), jpgFiles);
+                } else {
+                    String fileName = file.getName();
+                    if (fileName.endsWith(".jpg")) {
+                        jpgFiles.add(file.getAbsolutePath());
+                    }
+                }
+            }
+        }
     }
 
     //
@@ -36,7 +78,6 @@ public class test {
         }catch (IOException e){
             e.printStackTrace();
         }
-
         return "";
     }
 
